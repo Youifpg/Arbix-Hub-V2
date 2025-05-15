ad06bd52c371a357118d79dac403bd43aa52a4ee
ARBIX <youssefalsadick@gmail.com>
2025-05-16 01:48:37 +0300

    Update Source.lua

--- Source.lua
+++ Source.lua
@@ -21,10 +21,71 @@ Window:AddMinimizeButton({
 
 local Tab1 = Window:MakeTab({"Auto Things", "Home"})
 
-local Section = Tab1:AddSection({"Auto Collect [ Not Finished ]"})
+local Section = Tab1:AddSection({"Auto Plant"})
 
-local Toggle1 = Tab1:AddToggle({
-    Name = "Auto Collect",
-    Description = "",
-    Default = false
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local SeedLooping = false
+
+local function GetRandomPositionOnPart(MyFarm)
+    local Locations = MyFarm.Important.Plant_Locations:GetChildren()
+    local Part = Locations[math.random(1, #Locations)]
+
+    local RandomOffset = Vector3.new(
+        (math.random() - 0.5) * Part.Size.X,
+        (math.random() - 0.5) * Part.Size.Y,
+        (math.random() - 0.5) * Part.Size.Z
+    )
+
+    return Part.CFrame:PointToWorldSpace(RandomOffset)
+end
+
+local function GetOwnedSeed(Client)
+    local Character = Client.Character
+    local Backpack = Client:FindFirstChildOfClass("Backpack")
+
+    for _, v in ipairs(Character:GetChildren()) do
+        if v:IsA("Tool") and v:FindFirstChild("Plant_Name") then
+            return v
+        end
+    end
+
+    for _, v in ipairs(Backpack:GetChildren()) do
+        if v:IsA("Tool") and v:FindFirstChild("Plant_Name") then
+            v.Parent = Character
+            return v
+        end
+    end
+
+    return nil
+end
+
+local function AutoPlantSeed()
+    local MyFarm = findMyFarm()
+    if not MyFarm then return end
+
+    local Seed = GetOwnedSeed(Client)
+    local Root = Client.Character and Client.Character:FindFirstChild("HumanoidRootPart")
+    if Seed and Root then
+        Root.AssemblyLinearVelocity = Vector3.zero
+        local pos = GetRandomPositionOnPart(MyFarm)
+        Root.CFrame = CFrame.new(pos + Vector3.new(0, -5, 0))
+        task.wait(0.2)
+        ReplicatedStorage:WaitForChild("GameEvents"):WaitForChild("Plant_RE"):FireServer(pos, Seed)
+    end
+end
+
+Tab1:AddToggle({
+    Name = "Auto Plant Seed",
+    Default = false,
+    Callback = function(value)
+        SeedLooping = value
+        if SeedLooping then
+            spawn(function()
+                while SeedLooping do
+                    AutoPlantSeed()
+                    wait(0.5)
+                end
+            end)
+        end
+    end
 })
